79a89f3cebf384b72ecfd729beb3ef363d8bfde6
Richard Vinueza <richardvinueza@gmail.com>
2020-04-14 13:37:17 +0100

    home.page.ts code v1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MuseumService, localhost, Exhibitions, Artworks } from '../services/museum.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,63 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  locahost = localhost;
+
+  exhibitArray: Array<Exhibitions> = [];
+  exhibit: Exhibitions;
+
+  artArray: Array<Artworks> = [];
+  artArrayShow = new Array<Artworks>();
+  art: Artworks;
+  imgArtwork: any;
+
+  typeFileChoices: [string];
+
+  constructor(
+    private apiMuseum: MuseumService
+  ) { }
+
+  ionViewDidEnter() {
+    this.getExhibitions();
+    this.getArtworks();
+  }
+
+  getExhibitions() {
+    this.apiMuseum.getExhibitionsFromBackEnd().subscribe((res: Array<Exhibitions>) => {
+      this.exhibitArray = res;
+    })
+  }
+
+  getArtworks() {
+    this.apiMuseum.getArtworksFromBackEnd().subscribe((res: Array<Artworks>) => {
+      this.artArray = res;
+    })
+  }
+
+  changeTypeFile(event) {
+    let fileChoice: [string] = event.detail.value;
+    if (fileChoice == []) {
+      this.typeFileChoices = ["image"];
+      this.artArrayShow = this.artArray;
+    } else {
+      this.loadArtworkShow(fileChoice);
+    }
+  }
+
+  loadArtworkShow(fileChoice: [string]) {
+    this.artArrayShow = new Array<Artworks>();
+    for (let art of this.artArray) {
+      let mediaShow = false;
+      for (let media of art.media) {
+        if (fileChoice.includes(media.fileType)) {
+          mediaShow = true;
+        }
+      }
+      if (mediaShow == true) {
+        this.artArrayShow.push(art);
+      }
+    }
+    console.log(this.artArrayShow);
+  }
 
 }

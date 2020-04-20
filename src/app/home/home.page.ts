63fe9a49036465ffe1f56024e191e0e568011175
Richard Vinueza <richardvinueza@gmail.com>
2020-04-20 19:11:59 +0100

    Changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -16,6 +16,7 @@ export class HomePage {
   beaconArray: Array<Beacons> = [];
   auxDevice: any;
   beacon: Beacons;
+  bluetoothState: string;
 
   exhibitArray: Array<Exhibitions> = [];
   exhibit: Exhibitions;
@@ -35,6 +36,7 @@ export class HomePage {
 
   ionViewDidEnter() {
     this.getExhibitions();
+    this.getBeacons();
     this.isEnabled();
   }
 
@@ -43,15 +45,16 @@ export class HomePage {
   isEnabled() {
     this.ble.startStateNotifications().subscribe(state => {
       console.log("Bluetooth is " + state);
-      if (state == 'on') {
-        this.getBeacons();
+      this.bluetoothState = state;
+      if (this.bluetoothState == 'on') { 
         this.scanForBeacons();
-      } else if (state == 'off') {
-        
+
+      } else if (this.bluetoothState == 'off') {
         this.presentAlert();
       }
     });
   }
+
 
   async presentAlert() {
     const alert = await this.alertController.create({
